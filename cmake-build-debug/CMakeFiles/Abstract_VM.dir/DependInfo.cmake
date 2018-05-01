bf5fc25cb8b47422b4a5fe111f1050b4519c58bc
Arxcel <vadim.k.394010@gmail.com>
2018-05-01 10:34:53 +0300

    Add clion files

---
+++ cmake-build-debug/CMakeFiles/Abstract_VM.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/arxcel/Documents/projects/Abstract_VM/src/CustomException.cpp" "/Users/arxcel/Documents/projects/Abstract_VM/cmake-build-debug/CMakeFiles/Abstract_VM.dir/src/CustomException.cpp.o"
+  "/Users/arxcel/Documents/projects/Abstract_VM/src/FOperand.cpp" "/Users/arxcel/Documents/projects/Abstract_VM/cmake-build-debug/CMakeFiles/Abstract_VM.dir/src/FOperand.cpp.o"
+  "/Users/arxcel/Documents/projects/Abstract_VM/src/main.cpp" "/Users/arxcel/Documents/projects/Abstract_VM/cmake-build-debug/CMakeFiles/Abstract_VM.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Abstract_VM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Abstract_VM.dir/src/main.cpp.o"
+  "CMakeFiles/Abstract_VM.dir/src/CustomException.cpp.o"
+  "CMakeFiles/Abstract_VM.dir/src/FOperand.cpp.o"
+  "Abstract_VM.pdb"
+  "Abstract_VM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Abstract_VM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
